4afb73ba184d89db7eab7ed1f6352e975afed497
Julien Claivaz <julienclaivaz@juliens-mbp.home>
2023-02-24 22:39:35 +0100

    added functions for classes and user input

--- index.js
+++ index.js
@@ -13,3 +13,135 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let team = [];
+
+async function chooseStaff(){
+let {staff} = await inquirer
+
+.prompt([
+    {   type: 'list',
+        name: 'staff',
+        message: 'Choose one staff for your project',
+        choices: ['Manager', 'Engineer', 'Intern', 'Finished building the team'],
+
+    },
+
+    ])
+
+    function generateTeam(staff){
+        if(staff === 'Manager'){
+        chooseManager()
+    }else if(staff === 'Engineer'){
+        chooseEngineer()
+        
+    }else if(staff === 'Intern'){
+        chooseIntern()
+
+    }else if(staff === 'Finished building the team'){
+        console.log(team)
+    }
+    }
+    generateTeam(staff)
+
+}
+
+
+// Manager function *****************************
+
+async function chooseManager(){
+    let {name, id, email, officeNumber} = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your manager name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your manager id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your manager email?",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is your manager office number?",
+        },])
+        chooseStaff()
+    return team.push(new Manager (name, id, email, officeNumber));
+        
+}
+
+
+// Engineer function *****************************
+
+async function chooseEngineer(){
+    let {name, id, email, github} = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your engineer name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your engineer id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your engineer email?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is your engineer gitHub profile?",
+        },])
+        chooseStaff()
+    return team.push(new Engineer (name, id, email, github));
+        
+}
+
+// Intern function *****************************
+
+async function chooseIntern(){
+    let {name, id, email, school} = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is your intern name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is your intern id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is your intern email?",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is your intern's school?",
+        },])
+        chooseStaff()
+    return team.push(new Intern (name, id, email, school));
+        
+}
+
+chooseStaff();
+
+
+
+
+
+
+    // team.push(new Manager())
